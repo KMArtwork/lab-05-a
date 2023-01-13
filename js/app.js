ad759a71ef26ff44fd836dbbd950ac1118561ece
KMART <93807261+KMArtwork@users.noreply.github.com>
2023-01-13 14:07:58 -0800

    revert main back to clean slate, forgot to create new branch

--- js/app.js
+++ js/app.js
@@ -114,10 +114,7 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiplyArray(multArr) { //eslint-disable-line
-    let answers = [];
-    answers[0] = multiply(multiply(multArr[0], multArr[1])[0], multArr[2])[0];
-    answers[1] = `The numbers ${multArr[0]},${multArr[1]},${multArr[2]} have a product of ${answers[0]}.`;
-    return answers;
+   
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
