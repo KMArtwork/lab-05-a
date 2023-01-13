64b4065ad567b658bb7a71d12540a8fd64d8f797
KMART <93807261+KMArtwork@users.noreply.github.com>
2023-01-13 11:39:03 -0800

    complete first function

--- js/app.js
+++ js/app.js
@@ -9,11 +9,15 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sum(a, b) { //eslint-disable-line
-
+    let answers = [];
+    answers[0] = a + b;
+    answers[1] = `The sum of ${a} and ${b} is ${answers[0]}.`;
+    // console.log(answers)
+    return answers;
 }
 
 // Here is the test for sum(); uncomment it to run it
-// testSum(4, 7);
+testSum(4, 7);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
